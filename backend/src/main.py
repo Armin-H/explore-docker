3fc9270e8b194d254aae5a1feb7dbb5c936db407
Armin-H <armin.hejazian@yahoo.com>
2026-02-17 19:06:46 +1100

    added project name as enviornment variable to env.sample and updated .gitignore to ignore .env

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,12 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+PORJECT_NAME = os.environ.get("PROJECT_NAME")
+if not PORJECT_NAME:
+    raise ValueError("PROJECT_NAME is not set")
+
 @app.get("/")
 def get_root():
-    return {"message": "Hello World, from backend"}
+    return {"message": f"Hello World, from backend, Project Name: {PORJECT_NAME}"}
